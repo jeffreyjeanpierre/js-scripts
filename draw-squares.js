eee66f2a37dba5d9fb62e1a7815be989116f16e9
jeffreyjeanpierre <jitbag@gmail.com>
2019-06-19 08:36:54 -0400

    Updated with comments for future features

--- draw-squares.js
+++ draw-squares.js
@@ -1,12 +1,15 @@
 lastColor = ""
 alternateDefault = false;
-colorList = ["black", "black", "black", "white", "black", "white", "white", "white", "black", "white"]
-rectWidth = 20
-rectHeight = 20
+colorList = ["black", "black", "black", "white", "black", "white", "white", "black", "black", "white"]
+rectWidth = 10
+rectHeight = 10
 canvasId = 'pattern-board'
 displayId = 'pattern-score'
 serialId = 'pattern-code'
-
+cols = canvas.height/rectHeight
+rows = canvas.width/rectWidth
+imageWidth = window.innerWidth /5
+imageHeight = window.innerHeight /5
 
 randomStyle = (colorList) => {
     let colorIndex = Math.floor(Math.random(0,1) * colorList.length)
@@ -38,10 +41,27 @@ drawPattern = (rows, cols) => {
         drawColumns(row, cols, ctx);
     }
 }
+
+// create a map of what the "block" will look like
+// as the drawPattern iterates, if the row/col is in this map
+// then instead of drawing a pattern as expected,
+// it should draw what is mapped in the "block"
+// example: a row of 5 columns is expecting a 2 column "block" in "row 1, column 3" and "row 1, column 4"
+// therefore when we're on those corresponding coordinates, instead of printing the random color, print the "block"
+// this should result in an "image" that contains precalculated "blocks" and should theoretically be configurable for "patterns"
+// in other words, the output of the pattern is called an image (the whole thing) which contains blocks
+// ideally the block builder should be able to define patterns programatically,
+// such as adding a border around the image
+// or creating white squares that are 2x2 rectangle width wide
+// and from now on refer to rectangle widths as rexels (portmanteau for rectangular pixels) 
+blockBuilder = () => {}
+checkBlock = () => {}
+addImageBorder = () => {}
+
 var canvas = document.createElement('canvas')
 canvas.id = canvasId
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = imageWidth
+canvas.height = imageHeight
 
 var display = document.createElement('display')
 display.id = displayId
@@ -58,7 +78,7 @@ body.appendChild(canvas)
 body.appendChild(display)
 body.appendChild(serial)
 
-draw = () => { drawPattern(canvas.width/rectWidth, canvas.height/rectHeight) }
+draw = () => { drawPattern(rows, cols) }
 clickHandler = document.getElementById(canvasId).addEventListener('click', draw)
 document.addEventListener('keydown', (e) => { 
     if (e.key && (e.key == '32' || e.keyCode == '32')) draw()
